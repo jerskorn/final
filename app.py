65e27a4d468cdd11b5f22879bde08a9a3ee2bfaf
jerskorn <jerskorn@gmail.com>
2023-12-20 11:15:23 -0700

    version 1.  saving files and output csv

--- app.py
+++ app.py
@@ -1,19 +1,76 @@
 import csv
+import os
+from flask import Flask, redirect, render_template, request, url_for, send_from_directory
 from io import StringIO
-from flask import Flask, flash, redirect, render_template, request, session
-from fileinput import filename 
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'C:\Final Project\output'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'}
 
 app = Flask(__name__)
 
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/",methods=["GET", "POST"])
 def home():
-    if request.method == "GET":
+     if request.method == "GET":
          return render_template("final.html")
-    f = request.files['file'] 
-    data = StringIO(f.read().decode('utf-8'))  
-    list1 = []
-    name = csv.DictReader(data)
-    for row in name:
-         list1.append(row)
-    return render_template("success.html", name=list1)
+     # Get file to be uploaded
+     f = request.files['file'] 
+     # if valid 
+     if f and allowed_file(f.filename):
+          filename = secure_filename(f.filename)
+          
+          # get file names to split csv into good and bad data
+          bad_filename = f'{filename.split(".")[0]}_bad.csv'
+          good_filename = f'{filename.split(".")[0]}_good.csv'
+          
+          # make sure data is of string type
+          data = StringIO(f.read().decode('utf-8-sig')) 
+          
+          # get info to dictionary 
+          rows = csv.DictReader(data)
+          
+          # Get keys of dictionary
+          keys = rows.fieldnames
+          
+          # Open two files to write good and bad data
+          with open(os.path.join('output', good_filename), 'w', newline='') as g, open(os.path.join('output', bad_filename), 'w', newline='') as b :
+               
+               # get writer and writer header row for good
+               g_writer = csv.DictWriter(g, keys)
+               g_writer.writeheader()
+               
+               # get writer and writer header row for bad
+               b_writer = csv.DictWriter(b, keys)
+               b_writer.writeheader()
+               
+               # if the start time zeros bad data write to file
+               for row in rows:
+                    if row["START TIME"] == "0:00:00":
+                         b_writer.writerow(row)
+                    # else it's good data write to file     
+                    else:
+                         g_writer.writerow(row)
+                        
+          
+               # save good and bad files to output folder
+               f.save(os.path.join(app.config['UPLOAD_FOLDER'], good_filename))
+               f.save(os.path.join(app.config['UPLOAD_FOLDER'], bad_filename))
+          # auto download good file
+          return redirect(url_for('download_file', name=good_filename))     
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+
+
+
+
