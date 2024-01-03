2ff55d51d20777421f78cefc3deb8e9d9229111f
jerskorn <jerskorn@gmail.com>
2024-01-03 16:34:19 -0700

    Fixed Summery file and display

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ summary_list = []
 def summary(row):
      time = row["IDLE TIME (HH:MM:SS)"]
      (h, m, s) = time.split(':')
-     decimal_idle = int(h) + int(m)/60 + int(s)/3600
+     decimal_idle = int(h) + float(m)/60 + float(s)/3600
      time = row["ENGINE ON TIME (HH:MM:SS)"]
      (h, m, s) = time.split(':')
-     decimal_on = int(h) + int(m)/60 + int(s)/3600
+     decimal_on = int(h) + float(m)/60 + float(s)/3600
      percentrun = decimal_idle/decimal_on * 100
      dictrow = {'VEHICLE':row["VEHICLE"],'DRIVER NAME':row["DRIVER NAME"],
                     'Sum of Idle Hours':round(decimal_idle, 2), 
@@ -30,7 +30,8 @@ def summary(row):
                if dictrows["VEHICLE"] == dictrow["VEHICLE"]:
                     dictrows["Sum of Idle Hours"] = round(dictrows["Sum of Idle Hours"] + dictrow["Sum of Idle Hours"], 2)
                     dictrows["Sum of Run Hours"] = round(dictrows["Sum of Run Hours"] + dictrow["Sum of Run Hours"], 2)
-                    dictrows["Sum of Idle Percentage"] = round(int(dictrows["Sum of Idle Hours"]) / int(dictrows["Sum of Run Hours"]) * 100, 2) 
+                    if float(dictrows["Sum of Run Hours"]) > 0:
+                         dictrows["Sum of Idle Percentage"] = round(float(dictrows["Sum of Idle Hours"]) / float(dictrows["Sum of Run Hours"]) * 100, 2) 
                     res = 'yes'
                     break
      if res == None:
@@ -84,26 +85,41 @@ def home():
                for row in rows:
                     if row["START TIME"] == "0:00:00" or row["END TIME"] == "0:00:00":
                          b_writer.writerow(row)
-                         # for fun and trying to figure out time
-                         summary(row)
-                    # else it's good data write to file     
+                         
+                    # else it's good data write to file and summarize     
                     else:
                          g_writer.writerow(row)
+                         summary(row)
           
-          #write the summary file
+          # Total sum the summery lol
+          sih = 0
+          srh = 0
+          for row in summary_list:
+               sih += row["Sum of Idle Hours"]
+               srh += row["Sum of Run Hours"]
+          sip = sih / srh * 100
+          sums = {'VEHICLE':'','DRIVER NAME':'TOTALS:',
+                    'Sum of Idle Hours':round(sih, 2), 
+                    'Sum of Run Hours':round(srh, 2),
+                    'Sum of Idle Percentage':round(sip, 2)}
+          summary_list.append(sums)
+          
+          #write the summary list to file
+                         
+          # Put together field names for csv file
           fieldnames = ['VEHICLE', 'DRIVER NAME', 
                     'Sum of Idle Hours', 
                     'Sum of Run Hours',
                     'Sum of Idle Percentage']
+          
+          #Open new file and write summary list
           with open(os.path.join(app.config['UPLOAD_FOLDER'], good_summary), 'w', newline='') as s:
                s_writer = csv.DictWriter(s, fieldnames=fieldnames)
                s_writer.writeheader()
                s_writer.writerows(summary_list) 
-
-          for items in summary_list:
-               print(items)            
-          # auto download good file
-          return render_template("success.html", good_filename=good_filename, bad_filename=bad_filename, good_summary=good_summary)
+          
+          # We did it.  Return success page and offer downloads
+     return render_template("success.html", good_filename=good_filename, bad_filename=bad_filename, good_summary=good_summary)
 
 
     
